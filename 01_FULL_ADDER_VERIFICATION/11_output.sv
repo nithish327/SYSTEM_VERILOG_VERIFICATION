4b403c2f37552f6051b12a785c76f187b12cef25
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-05-16 20:21:21 +0530

    Add output verification for full adder test cases

---
+++ 01_FULL_ADDER_VERIFICATION/11_output.sv
@@ -0,0 +1,49 @@
+xcelium> source /xcelium25.03/tools/xcelium/files/xmsimrc
+xcelium> run
+EXPECTED OUTPUT : Time = 11 sum = 0 cout = 0
+ACTUAL OUTOUT : Time = 11  sum = 0 cout = 0
++---------+
+|   PASS  |
++---------+
+----------------------------
+EXPECTED OUTPUT : Time = 22 sum = 1 cout = 0
+ACTUAL OUTOUT : Time = 22  sum = 1 cout = 0
++---------+
+|   PASS  |
++---------+
+----------------------------
+EXPECTED OUTPUT : Time = 33 sum = 1 cout = 0
+ACTUAL OUTOUT : Time = 33  sum = 1 cout = 0
++---------+
+|   PASS  |
++---------+
+----------------------------
+EXPECTED OUTPUT : Time = 44 sum = 0 cout = 1
+ACTUAL OUTOUT : Time = 44  sum = 0 cout = 1
++---------+
+|   PASS  |
++---------+
+----------------------------
+EXPECTED OUTPUT : Time = 55 sum = 1 cout = 0
+ACTUAL OUTOUT : Time = 55  sum = 1 cout = 0
++---------+
+|   PASS  |
++---------+
+----------------------------
+EXPECTED OUTPUT : Time = 66 sum = 0 cout = 1
+ACTUAL OUTOUT : Time = 66  sum = 0 cout = 1
++---------+
+|   PASS  |
++---------+
+----------------------------
+EXPECTED OUTPUT : Time = 77 sum = 0 cout = 1
+ACTUAL OUTOUT : Time = 77  sum = 0 cout = 1
++---------+
+|   PASS  |
++---------+
+----------------------------
+EXPECTED OUTPUT : Time = 88 sum = 1 cout = 1
+ACTUAL OUTOUT : Time = 88  sum = 1 cout = 1
++---------+
+|   PASS  |
++---------+
