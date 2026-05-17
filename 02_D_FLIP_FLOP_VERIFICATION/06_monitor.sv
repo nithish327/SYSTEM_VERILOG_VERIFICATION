96a5c52328fb38fc7f0ef59510ed455a4a33f520
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-05-17 20:08:39 +0530

    Create 06_monitor.sv

---
+++ 02_D_FLIP_FLOP_VERIFICATION/06_monitor.sv
@@ -0,0 +1,26 @@
+class monitor;
+  mailbox m2s;
+  event done;
+  virtual dff_if vif;
+
+  function new(mailbox m2s,virtual dff_if vif,event done);
+    this.m2s = m2s;
+    this.vif = vif;
+    this.done = done;
+  endfunction
+  
+  task run();
+    transaction tr;
+    repeat (20)begin
+      @( posedge vif.clk);
+      #1;
+      tr = new();
+      tr.d = vif.d;
+      tr.reset = vif.reset;
+      tr.q = vif.q;
+      m2s.put(tr);
+      ->done;
+      $display("MONITOR   : Time =%0t d = %0d q = %0d",$time,vif.d,vif.q);
+    end
+  endtask
+endclass
