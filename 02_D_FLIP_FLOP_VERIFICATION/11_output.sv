a7c9afb35bd012059308781b7297535c0d5aa625
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-05-17 20:13:27 +0530

    Create 11_output.sv

---
+++ 02_D_FLIP_FLOP_VERIFICATION/11_output.sv
@@ -0,0 +1,184 @@
+xcelium> source /xcelium25.03/tools/xcelium/files/xmsimrc
+xcelium> run
+GENERATOR : Time =15 d = 0
+DRIVER    : Time =20 d = 0
+MONITOR   : Time =26 d = 0 q = 0
+EXPECTED OUTPUT : Time = 26 d = 0 q = 0
+ACTUAL OUTPUT   : Time = 26 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =26 d = 0
+DRIVER    : Time =30 d = 0
+MONITOR   : Time =36 d = 0 q = 0
+EXPECTED OUTPUT : Time = 36 d = 0 q = 0
+ACTUAL OUTPUT   : Time = 36 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =36 d = 0
+DRIVER    : Time =40 d = 0
+MONITOR   : Time =46 d = 0 q = 0
+EXPECTED OUTPUT : Time = 46 d = 0 q = 0
+ACTUAL OUTPUT   : Time = 46 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =46 d = 0
+DRIVER    : Time =50 d = 0
+MONITOR   : Time =56 d = 0 q = 0
+EXPECTED OUTPUT : Time = 56 d = 0 q = 0
+ACTUAL OUTPUT   : Time = 56 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =56 d = 1
+DRIVER    : Time =60 d = 1
+MONITOR   : Time =66 d = 1 q = 0
+EXPECTED OUTPUT : Time = 66 d = 1 q = 0
+ACTUAL OUTPUT   : Time = 66 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =66 d = 1
+DRIVER    : Time =70 d = 1
+MONITOR   : Time =76 d = 1 q = 1
+EXPECTED OUTPUT : Time = 76 d = 1 q = 1
+ACTUAL OUTPUT   : Time = 76 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =76 d = 0
+DRIVER    : Time =80 d = 0
+MONITOR   : Time =86 d = 0 q = 1
+EXPECTED OUTPUT : Time = 86 d = 0 q = 1
+ACTUAL OUTPUT   : Time = 86 d = 0 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =86 d = 1
+DRIVER    : Time =90 d = 1
+MONITOR   : Time =96 d = 1 q = 0
+EXPECTED OUTPUT : Time = 96 d = 1 q = 0
+ACTUAL OUTPUT   : Time = 96 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =96 d = 1
+DRIVER    : Time =100 d = 1
+MONITOR   : Time =106 d = 1 q = 1
+EXPECTED OUTPUT : Time = 106 d = 1 q = 1
+ACTUAL OUTPUT   : Time = 106 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =106 d = 1
+DRIVER    : Time =110 d = 1
+MONITOR   : Time =116 d = 1 q = 1
+EXPECTED OUTPUT : Time = 116 d = 1 q = 1
+ACTUAL OUTPUT   : Time = 116 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =116 d = 0
+DRIVER    : Time =120 d = 0
+MONITOR   : Time =126 d = 0 q = 1
+EXPECTED OUTPUT : Time = 126 d = 0 q = 1
+ACTUAL OUTPUT   : Time = 126 d = 0 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =126 d = 1
+DRIVER    : Time =130 d = 1
+MONITOR   : Time =136 d = 1 q = 0
+EXPECTED OUTPUT : Time = 136 d = 1 q = 0
+ACTUAL OUTPUT   : Time = 136 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =136 d = 0
+DRIVER    : Time =140 d = 0
+MONITOR   : Time =146 d = 0 q = 1
+EXPECTED OUTPUT : Time = 146 d = 0 q = 1
+ACTUAL OUTPUT   : Time = 146 d = 0 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =146 d = 0
+DRIVER    : Time =150 d = 0
+MONITOR   : Time =156 d = 0 q = 0
+EXPECTED OUTPUT : Time = 156 d = 0 q = 0
+ACTUAL OUTPUT   : Time = 156 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =156 d = 0
+DRIVER    : Time =160 d = 0
+MONITOR   : Time =166 d = 0 q = 0
+EXPECTED OUTPUT : Time = 166 d = 0 q = 0
+ACTUAL OUTPUT   : Time = 166 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =166 d = 1
+DRIVER    : Time =170 d = 1
+MONITOR   : Time =176 d = 1 q = 0
+EXPECTED OUTPUT : Time = 176 d = 1 q = 0
+ACTUAL OUTPUT   : Time = 176 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =176 d = 1
+DRIVER    : Time =180 d = 1
+MONITOR   : Time =186 d = 1 q = 1
+EXPECTED OUTPUT : Time = 186 d = 1 q = 1
+ACTUAL OUTPUT   : Time = 186 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =186 d = 1
+DRIVER    : Time =190 d = 1
+MONITOR   : Time =196 d = 1 q = 1
+EXPECTED OUTPUT : Time = 196 d = 1 q = 1
+ACTUAL OUTPUT   : Time = 196 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =196 d = 1
+DRIVER    : Time =200 d = 1
+MONITOR   : Time =206 d = 1 q = 1
+EXPECTED OUTPUT : Time = 206 d = 1 q = 1
+ACTUAL OUTPUT   : Time = 206 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =206 d = 0
+DRIVER    : Time =210 d = 0
+MONITOR   : Time =216 d = 0 q = 1
+EXPECTED OUTPUT : Time = 216 d = 0 q = 1
+ACTUAL OUTPUT   : Time = 216 d = 0 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+Simulation complete via $finish(1) at time 416 NS + 0
+./testbench.sv:31     $finish;
