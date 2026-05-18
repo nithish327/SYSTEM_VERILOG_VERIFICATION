cd99352f1d2213ea0d7478e2d25fc173d9ccddee
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-05-18 21:49:48 +0530

    Add output logs for asynchronous D flip-flop tests
    The output file contains logs of test cases for the asynchronous D flip-flop verification, showing expected and actual outputs at various times. All test cases have passed successfully.

---
+++ 03_ASYNCHRONOUS_D_FLIP_FLOP_VERIFICATION/10_output.sv
@@ -0,0 +1,181 @@
+xcelium> source /xcelium25.03/tools/xcelium/files/xmsimrc
+xcelium> run
+GENERATOR : Time =15 d = 0
+DRIVER    : Time =15 d = 0
+MONITOR   : Time =16 d = 0 q = 0
+EXPECTED OUTPUT : Time = 16 d = 0 q = 0
+ACTUAL OUTPUT   : Time = 16 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =16 d = 0
+DRIVER    : Time =25 d = 0
+MONITOR   : Time =26 d = 0 q = 0
+EXPECTED OUTPUT : Time = 26 d = 0 q = 0
+ACTUAL OUTPUT   : Time = 26 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =26 d = 0
+DRIVER    : Time =35 d = 0
+MONITOR   : Time =36 d = 0 q = 0
+EXPECTED OUTPUT : Time = 36 d = 0 q = 0
+ACTUAL OUTPUT   : Time = 36 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =36 d = 0
+DRIVER    : Time =45 d = 0
+MONITOR   : Time =46 d = 0 q = 0
+EXPECTED OUTPUT : Time = 46 d = 0 q = 0
+ACTUAL OUTPUT   : Time = 46 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =46 d = 1
+DRIVER    : Time =55 d = 1
+MONITOR   : Time =56 d = 1 q = 0
+EXPECTED OUTPUT : Time = 56 d = 1 q = 0
+ACTUAL OUTPUT   : Time = 56 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =56 d = 1
+DRIVER    : Time =65 d = 1
+MONITOR   : Time =66 d = 1 q = 1
+EXPECTED OUTPUT : Time = 66 d = 1 q = 1
+ACTUAL OUTPUT   : Time = 66 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =66 d = 0
+DRIVER    : Time =75 d = 0
+MONITOR   : Time =76 d = 0 q = 1
+EXPECTED OUTPUT : Time = 76 d = 0 q = 1
+ACTUAL OUTPUT   : Time = 76 d = 0 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =76 d = 1
+DRIVER    : Time =85 d = 1
+MONITOR   : Time =86 d = 1 q = 0
+EXPECTED OUTPUT : Time = 86 d = 1 q = 0
+ACTUAL OUTPUT   : Time = 86 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =86 d = 1
+DRIVER    : Time =95 d = 1
+MONITOR   : Time =96 d = 1 q = 1
+EXPECTED OUTPUT : Time = 96 d = 1 q = 1
+ACTUAL OUTPUT   : Time = 96 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =96 d = 1
+DRIVER    : Time =105 d = 1
+MONITOR   : Time =106 d = 1 q = 1
+EXPECTED OUTPUT : Time = 106 d = 1 q = 1
+ACTUAL OUTPUT   : Time = 106 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =106 d = 0
+DRIVER    : Time =115 d = 0
+MONITOR   : Time =116 d = 0 q = 1
+EXPECTED OUTPUT : Time = 116 d = 0 q = 1
+ACTUAL OUTPUT   : Time = 116 d = 0 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =116 d = 1
+DRIVER    : Time =125 d = 1
+MONITOR   : Time =126 d = 1 q = 0
+EXPECTED OUTPUT : Time = 126 d = 1 q = 0
+ACTUAL OUTPUT   : Time = 126 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =126 d = 0
+DRIVER    : Time =135 d = 0
+MONITOR   : Time =136 d = 0 q = 1
+EXPECTED OUTPUT : Time = 136 d = 0 q = 1
+ACTUAL OUTPUT   : Time = 136 d = 0 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =136 d = 0
+DRIVER    : Time =145 d = 0
+MONITOR   : Time =146 d = 0 q = 0
+EXPECTED OUTPUT : Time = 146 d = 0 q = 0
+ACTUAL OUTPUT   : Time = 146 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =146 d = 0
+DRIVER    : Time =155 d = 0
+MONITOR   : Time =156 d = 0 q = 0
+EXPECTED OUTPUT : Time = 156 d = 0 q = 0
+ACTUAL OUTPUT   : Time = 156 d = 0 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =156 d = 1
+DRIVER    : Time =165 d = 1
+MONITOR   : Time =166 d = 1 q = 0
+EXPECTED OUTPUT : Time = 166 d = 1 q = 0
+ACTUAL OUTPUT   : Time = 166 d = 1 q = 0
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =166 d = 1
+DRIVER    : Time =175 d = 1
+MONITOR   : Time =176 d = 1 q = 1
+EXPECTED OUTPUT : Time = 176 d = 1 q = 1
+ACTUAL OUTPUT   : Time = 176 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =176 d = 1
+DRIVER    : Time =185 d = 1
+MONITOR   : Time =186 d = 1 q = 1
+EXPECTED OUTPUT : Time = 186 d = 1 q = 1
+ACTUAL OUTPUT   : Time = 186 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =186 d = 1
+DRIVER    : Time =195 d = 1
+MONITOR   : Time =196 d = 1 q = 1
+EXPECTED OUTPUT : Time = 196 d = 1 q = 1
+ACTUAL OUTPUT   : Time = 196 d = 1 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
+-----------------
+GENERATOR : Time =196 d = 0
+DRIVER    : Time =205 d = 0
+MONITOR   : Time =206 d = 0 q = 1
+EXPECTED OUTPUT : Time = 206 d = 0 q = 1
+ACTUAL OUTPUT   : Time = 206 d = 0 q = 1
++------------------+
+|  TEST CASE PASS  |
++------------------+
