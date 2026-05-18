e4216620162329ab0b16f9049088f64ade889a2a
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-05-18 21:48:44 +0530

    Create 09_testbench.sv

---
+++ 03_ASYNCHRONOUS_D_FLIP_FLOP_VERIFICATION/09_testbench.sv
@@ -0,0 +1,37 @@
+`include "transaction.sv"
+`include "interface.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+`include "environment.sv"
+
+module tb;
+  
+  dff_if vif();
+  
+  d_ff dut (.clk(vif.clk), .reset(vif.reset), .d(vif.d), .q(vif.q));
+  
+  environment env;
+  
+  always #5 vif.clk = ~vif.clk;
+  
+  initial begin
+
+    vif.clk = 0;
+    vif.reset = 1;
+    vif.d = 0;
+    repeat(2)@(posedge vif.clk);
+    vif.reset = 0;
+    
+    env = new(vif);
+    env.run();
+    #200;
+    $finish;
+  end
+  initial begin 
+        
+    $dumpfile("dff.vcd");
+    $dumpvars(0,tb);
+  end
+endmodule
